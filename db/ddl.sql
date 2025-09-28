899d74d31aff6b464ec8c0efc174c55c2aedab86
parham <parhamsaghazadeh@gmail.com>
2025-09-28 22:28:26 +0330

    create table person

--- db/ddl.sql
+++ db/ddl.sql
@@ -18,3 +18,17 @@ create table if not exists role (
     id int auto_increment primary key ,
     title varchar(100)
 );
+
+create table if not exists person (
+    id int auto_increment primary key ,
+    name varchar(100),
+    lastname varchar(100),
+    birthdate timestamp,
+    national int,
+    user_id int,
+    gender_id int,
+    role_id int ,
+    constraint Fk_person_user foreign key (user_id) references user(id),
+    constraint FK_person_gender foreign key (gender_id) references gender(id),
+    constraint Fk_person_role foreign key (role_id) references role(id)
+);
