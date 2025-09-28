4d1aee48bd31a4438fe99bed0f6687fde40dfeeb
parham <parhamsaghazadeh@gmail.com>
2025-09-28 22:09:47 +0330

    create table user

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,3 +1,10 @@
 create database shop;
 use shop;
 
+create table if not exists user
+(
+    id       int auto_increment primary key,
+    username varchar(100) not null,
+    password varchar(100) not null,
+    constraint username unique (username)
+);
