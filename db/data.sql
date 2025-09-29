71179978f6a55dbb3f4f95913b547acd9fee3a29
parham <parhamsaghazadeh@gmail.com>
2025-09-29 19:03:38 +0330

    insert data person

--- db/data.sql
+++ db/data.sql
@@ -1,3 +1,4 @@
 insert into user value (1,'parham86','p123456');
 insert into gender value (1,'مرد '),(2,'زن');
 insert into role value (1,'buyer'),(2,'the seller');
+insert into person value (1,'parham','saghazadeh','2007-09-12','5100339160',1,1,2);
