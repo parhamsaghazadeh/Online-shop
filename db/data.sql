a66bc79928c6ce561714f1d1852ea8d3504a5f92
parham <parhamsaghazadeh@gmail.com>
2025-10-01 20:25:58 +0330

    change name table user

--- db/data.sql
+++ db/data.sql
@@ -1,4 +1,5 @@
-insert into user value (1,'parham86','p123456');
+insert into users value (1,'parham86','p123456');
 insert into gender value (1,'مرد '),(2,'زن');
 insert into role value (1,'buyer'),(2,'the seller');
 insert into person value (1,'parham','saghazadeh','2007-09-12','5100339160',1,1,2);
+rename table shop.user to users;
