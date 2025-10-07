6617f06ef3d7c8ab8482d710953d3f18f69a1b94
parham <parhamsaghazadeh@gmail.com>
2025-10-07 21:44:55 +0330

    add UserController GetMapping

---
+++ src/main/java/com/onlineShop/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.onlineShop.controller;
+
+import com.*;
+import com.onlineShop.factory.SBExampleApplication;
+import com.onlineShop.db.CrudRepository;
+import com.onlineShop.db.DatabaseConnection;
+import com.onlineShop.db.ResultSetHandler;
+import com.onlineShop.db.TransactionManager;
+import com.onlineShop.entity.User;
+import com.onlineShop.model.UserModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.onlineShop.model.Converter;
+
+import java.sql.Connection;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+    @Autowired
+    private Converter converter;
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        log.info("getAllUsers");
+        ArrayList<UserModel> userModel;
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            ResultSetHandler<User> userHandler = resultSet -> new User(
+                    resultSet.getLong("id"),
+                    resultSet.getString("username"),
+                    resultSet.getString("password")
+            );
+            CrudRepository<User> userRepository = new CrudRepository<>(connection, "users", userHandler);
+            List<User> users = userRepository.readAll("SELECT * FROM users");
+            userModel = users.stream().map(converter::converterToUser).peek(System.out::println)
+                    .collect(Collectors.toCollection(ArrayList::new));
+            return ResponseEntity.ok(userModel);
+        } catch (Exception e) {
+            log.error("getAllUsers{}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
