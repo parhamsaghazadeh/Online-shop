62cf0d2a58a960490f5650afa17b6a890adbefa7
parham <parhamsaghazadeh@gmail.com>
2025-10-07 21:44:25 +0330

    add converter user

---
+++ src/main/java/com/onlineShop/model/Converter.java
@@ -0,0 +1,26 @@
+package com.onlineShop.model;
+
+import com.onlineShop.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Converter {
+        public UserModel converterToUser(User user){
+                UserModel userModel = new UserModel();
+                userModel.setId(user.getId());
+                userModel.setUsername(user.getUsername());
+                userModel.setPassword(user.getPassword());
+                return userModel;
+        }
+
+        public User converterToUser(UserModel userModel){
+                if(userModel == null){
+                        return null;
+                }
+                return new User(
+                        userModel.getId(),
+                        userModel.getUsername(),
+                        userModel.getPassword()
+                );
+        }
+}
