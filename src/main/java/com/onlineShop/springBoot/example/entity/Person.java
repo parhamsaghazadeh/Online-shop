5f4c5cd3cf3fcea62460f09d0d43c60e845ec97d
parham <parhamsaghazadeh@gmail.com>
2025-10-08 18:38:31 +0330

    edit Person

---
+++ src/main/java/com/onlineShop/springBoot/example/entity/Person.java
@@ -0,0 +1,89 @@
+package com.onlineShop.springBoot.example.entity;
+
+import java.util.Date;
+
+public class Person {
+    private Long id;
+    private String name;
+    private String lastname;
+    private Date birthdate;
+    private String national_id;
+    private int user_id;
+    private int gender_id;
+    private int role_id;
+
+    public Person(Long id , String name,String lastname,Date birthdate,String national_id,int user_id,int gender_id,int role_id) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.national_id = national_id;
+        this.user_id = user_id;
+        this.gender_id = gender_id;
+        this.role_id = role_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getNational_id() {
+        return national_id;
+    }
+
+    public void setNational_id(String national_id) {
+        this.national_id = national_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getGender_id() {
+        return gender_id;
+    }
+
+    public void setGender_id(int gender_id) {
+        this.gender_id = gender_id;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+}
