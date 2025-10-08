33b5114966b079ae6ed7709fd0dfa7b1f2622dec
parham <parhamsaghazadeh@gmail.com>
2025-10-08 19:58:33 +0330

    add getMapping search

--- src/main/java/com/onlineShop/springBoot/example/controller/PersonController.java
+++ src/main/java/com/onlineShop/springBoot/example/controller/PersonController.java
@@ -1,4 +1,5 @@
 package com.onlineShop.springBoot.example.controller;
+
 import com.onlineShop.springBoot.example.db.DatabaseConnection;
 import com.onlineShop.springBoot.example.entity.Person;
 import com.onlineShop.springBoot.example.model.PersonModel;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,11 +30,12 @@ import java.sql.Connection;
 public class PersonController {
     @Autowired
     private Converter converter;
+
     @GetMapping
-    public ResponseEntity<List<PersonModel>> personAll(){
+    public ResponseEntity<List<PersonModel>> personAll() {
         log.info("Get all persons");
         ArrayList<PersonModel> personModels;
-        try (Connection connection = DatabaseConnection.getConnection()){
+        try (Connection connection = DatabaseConnection.getConnection()) {
             ResultSetHandler personHandler = resultSet -> new Person(
                     resultSet.getLong("id"),
                     resultSet.getString("name"),
@@ -44,14 +47,42 @@ public class PersonController {
                     resultSet.getLong("role_id")
             );
 
-            CrudRepository personRepository = new CrudRepository(connection,"person", personHandler);
+            CrudRepository personRepository = new CrudRepository(connection, "person", personHandler);
             List<Person> persons = personRepository.readAll("SELECT * FROM person ");
             personModels = persons.stream().map(converter::converterToPerson).peek(System.out::println)
                     .collect(Collectors.toCollection(ArrayList::new));
             return ResponseEntity.ok(personModels);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Error getting persons{}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<List<PersonModel>> searchPerson(@RequestParam Long id) {
+        log.info("Search person with id {}", id);
+        ArrayList<PersonModel> personModels;
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            ResultSetHandler personHandler = resultSet -> new Person(
+                    resultSet.getLong("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("lastname"),
+                    resultSet.getDate("birthdate"),
+                    resultSet.getString("national_id"),
+                    resultSet.getLong("user_id"),
+                    resultSet.getLong("gender_id"),
+                    resultSet.getLong("role_id")
+            );
+
+            CrudRepository personRepository = new CrudRepository(connection, "person", personHandler);
+            List<Person> persons = personRepository.readAll("SELECT * FROM person WHERE id = ? ");
+            personModels = persons.stream().map(converter::converterToPerson).
+                    collect(Collectors.toCollection(ArrayList::new));
+            return ResponseEntity.ok(personModels);
+        } catch (Exception e) {
+            log.error("Error getting persons{}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
