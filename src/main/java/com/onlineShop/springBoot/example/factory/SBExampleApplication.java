ee96ca6ad879366b20fdd166387c59c3fe920610
parham <parhamsaghazadeh@gmail.com>
2025-10-08 17:37:01 +0330

    add initializeSchema to SBExampleApplication

--- src/main/java/com/onlineShop/springBoot/example/factory/SBExampleApplication.java
+++ src/main/java/com/onlineShop/springBoot/example/factory/SBExampleApplication.java
@@ -1,14 +1,21 @@
 package com.onlineShop.springBoot.example.factory;
 import com.onlineShop.springBoot.example.db.DatabaseConnection;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+@Slf4j
 @SpringBootApplication(scanBasePackages = {"com.onlineShop"})
 
 public class SBExampleApplication {
     public static void main(String[] args) throws SQLException {
         SpringApplication.run(SBExampleApplication.class, args);
-        DatabaseConnection.getConnection();
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            DatabaseConnection.initializeSchema(connection);
+        }catch (SQLException e) {
+            log.error(e.getMessage());
+        }
     }
 }
