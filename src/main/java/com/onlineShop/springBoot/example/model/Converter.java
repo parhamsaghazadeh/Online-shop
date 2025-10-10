4a176b362b089c2bd48ad88c03a34d1631862297
parham <parhamsaghazadeh@gmail.com>
2025-10-10 15:15:10 +0330

    edit and fixing cod

--- src/main/java/com/onlineShop/springBoot/example/model/Converter.java
+++ src/main/java/com/onlineShop/springBoot/example/model/Converter.java
@@ -2,10 +2,15 @@ package com.onlineShop.springBoot.example.model;
 
 import com.onlineShop.springBoot.example.entity.Person;
 import com.onlineShop.springBoot.example.entity.*;
+import java.text.DecimalFormat;
+import java.math.BigDecimal;
+
+
 import com.onlineShop.springBoot.example.entity.User;
 import org.springframework.stereotype.Component;
 
 import java.sql.Date;
+import java.time.Year;
 
 @Component
 public class Converter {
@@ -57,6 +62,44 @@ public class Converter {
                 personModel.getUser_id(),
                 personModel.getGender_id(),
                 personModel.getRole_id()
+        );
+    }
+
+    private static final DecimalFormat df = new DecimalFormat("#,##0.00");
+    public ProductModel converterToProduct(Product product) {
+        if (product == null) {
+            return null;
+        }
+        ProductModel productModel = new ProductModel();
+        productModel.setId(product.getId());
+        productModel.setName(product.getName());
+        productModel.setBrand(product.getBrand());
+        productModel.setModel(product.getModel());
+        productModel.setMade_in(product.getMade_in());
+        productModel.setYear_of_manufacture(product.getYear_of_manufacture() != null ? product.getYear_of_manufacture().toString() : null);
+        productModel.setPrice(df.format(product.getPrice()));
+
+        productModel.setCategory_id(product.getCategory_id());
+        return productModel;
+    }
+
+    public Product converterToProductEntity(ProductModel productModel) {
+        if (productModel == null) {
+            return null;
+        }
+        Year year = Year.parse(productModel.getYear_of_manufacture());
+        BigDecimal price = new BigDecimal(productModel.getPrice().replace(",", ""));
+
+        return new Product(
+                productModel.getId(),
+                productModel.getName(),
+                productModel.getBrand(),
+                productModel.getModel(),
+                productModel.getMade_in(),
+                year,
+                productModel.getDesign(),
+                price,
+                productModel.getCategory_id()
         );
     }
 
