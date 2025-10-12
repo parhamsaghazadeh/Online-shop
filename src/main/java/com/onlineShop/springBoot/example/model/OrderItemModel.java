54ce6c2b6a7d5d7c2df302726a9e90d82d10232f
parham <parhamsaghazadeh@gmail.com>
2025-10-12 21:54:09 +0330

    add OrderItemModel

---
+++ src/main/java/com/onlineShop/springBoot/example/model/OrderItemModel.java
@@ -0,0 +1,49 @@
+package com.onlineShop.springBoot.example.model;
+
+public class OrderItemModel {
+    private Long id;
+    private Long order_id;
+    private Long product_id;
+    private Integer quantity;
+    private String price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Long order_id) {
+        this.order_id = order_id;
+    }
+
+    public Long getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Long product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
