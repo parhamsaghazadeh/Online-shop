1c86792cb48facdeaabea3c1bab7b24f8e0d7b7c
parham <parhamsaghazadeh@gmail.com>
2025-10-12 21:22:51 +0330

    add OrderModel

---
+++ src/main/java/com/onlineShop/springBoot/example/model/OrderModel.java
@@ -0,0 +1,40 @@
+package com.onlineShop.springBoot.example.model;
+
+public class OrderModel {
+    private long id;
+    private long person_id;
+    private String payment_method;
+    private String payment_date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(long person_id) {
+        this.person_id = person_id;
+    }
+
+    public String getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public String getPayment_date() {
+        return payment_date;
+    }
+
+    public void setPayment_date(String payment_date) {
+        this.payment_date = payment_date;
+    }
+}
