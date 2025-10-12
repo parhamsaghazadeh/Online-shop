5870ab1782ae10b7e38b50015841074475525010
parham <parhamsaghazadeh@gmail.com>
2025-10-12 22:04:52 +0330

    add OrderItem entity

---
+++ src/main/java/com/onlineShop/springBoot/example/entity/OrderItem.java
@@ -0,0 +1,58 @@
+package com.onlineShop.springBoot.example.entity;
+import java.math.BigDecimal;
+
+public class OrderItem {
+    private Long id;
+    private Long order_id;
+    private Long product_id;
+    private Integer quantity;
+    private BigDecimal price;
+
+    public OrderItem(Long id, Long order_id, Long product_id, Integer quantity, BigDecimal price) {
+        this.id = id;
+        this.order_id = order_id;
+        this.product_id = product_id;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Long order_id) {
+        this.order_id = order_id;
+    }
+
+    public Long getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Long product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
