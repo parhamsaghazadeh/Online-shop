09325c3d40ad64761a8618b6c1ce80f00c1e1d2c
parham <parhamsaghazadeh@gmail.com>
2025-10-16 14:27:17 +0330

    add OrderLocationModel

---
+++ src/main/java/com/onlineShop/springBoot/example/model/OrderLocationModel.java
@@ -0,0 +1,31 @@
+package com.onlineShop.springBoot.example.model;
+
+public class OrderLocationModel {
+    private Long id;
+    private Long order_id;
+    private Long location_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Long order_id) {
+        this.order_id = order_id;
+    }
+
+    public Long getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(Long location_id) {
+        this.location_id = location_id;
+    }
+}
