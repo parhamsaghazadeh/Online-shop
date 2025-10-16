05fd31b40ddbe797a113a182f815cc0f79bcab67
parham <parhamsaghazadeh@gmail.com>
2025-10-16 14:22:06 +0330

    add ProductRegistrationModel

---
+++ src/main/java/com/onlineShop/springBoot/example/model/ProductRegistrationModel.java
@@ -0,0 +1,10 @@
+package com.onlineShop.springBoot.example.model;
+
+import java.time.LocalDate;
+
+public class ProductRegistrationModel {
+    private Long id;
+    private String Person_id;
+    private String Product_id;
+    private LocalDate registration_date;
+}
