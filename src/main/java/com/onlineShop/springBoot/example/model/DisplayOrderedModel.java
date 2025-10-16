7e9892d50d13b91145eb48382b361c391f3394c1
parham <parhamsaghazadeh@gmail.com>
2025-10-16 14:56:53 +0330

    add DisplayOrderedModel

---
+++ src/main/java/com/onlineShop/springBoot/example/model/DisplayOrderedModel.java
@@ -0,0 +1,77 @@
+package com.onlineShop.springBoot.example.model;
+
+
+public class DisplayOrderedModel{
+    private ProductModel productModels;
+    private CategoryModel categoryModels;
+    private OrderItemModel orderItemModels;
+    private ProductRegistrationModel productRegistrationModels;
+    private OrderModel orderModels;
+    private PersonModel personModels;
+    private OrderLocationModel orderLocationModels;
+    private LocationModel locationModels;
+
+    public ProductModel getProductModels() {
+        return productModels;
+    }
+
+    public void setProductModels(ProductModel productModels) {
+        this.productModels = productModels;
+    }
+
+    public CategoryModel getCategoryModels() {
+        return categoryModels;
+    }
+
+    public void setCategoryModels(CategoryModel categoryModels) {
+        this.categoryModels = categoryModels;
+    }
+
+    public OrderItemModel getOrderItemModels() {
+        return orderItemModels;
+    }
+
+    public void setOrderItemModels(OrderItemModel orderItemModels) {
+        this.orderItemModels = orderItemModels;
+    }
+
+    public ProductRegistrationModel getProductRegistrationModels() {
+        return productRegistrationModels;
+    }
+
+    public void setProductRegistrationModels(ProductRegistrationModel productRegistrationModels) {
+        this.productRegistrationModels = productRegistrationModels;
+    }
+
+    public OrderModel getOrderModels() {
+        return orderModels;
+    }
+
+    public void setOrderModels(OrderModel orderModels) {
+        this.orderModels = orderModels;
+    }
+
+    public PersonModel getPersonModels() {
+        return personModels;
+    }
+
+    public void setPersonModels(PersonModel personModels) {
+        this.personModels = personModels;
+    }
+
+    public OrderLocationModel getOrderLocationModels() {
+        return orderLocationModels;
+    }
+
+    public void setOrderLocationModels(OrderLocationModel orderLocationModels) {
+        this.orderLocationModels = orderLocationModels;
+    }
+
+    public LocationModel getLocationModels() {
+        return locationModels;
+    }
+
+    public void setLocationModels(LocationModel locationModels) {
+        this.locationModels = locationModels;
+    }
+}
