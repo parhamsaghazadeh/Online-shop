e84e5b5209c5a7295046d51ccea9205d602d0ae9
parham <parhamsaghazadeh@gmail.com>
2025-10-16 14:30:29 +0330

    add Location

---
+++ src/main/java/com/onlineShop/springBoot/example/entity/Location.java
@@ -0,0 +1,49 @@
+package com.onlineShop.springBoot.example.entity;
+
+import java.time.LocalTime;
+
+public class Location {
+    private Long id;
+    private String title;
+    private String type;
+    private LocalTime open_time;
+
+    public Location(Long id, String title, String type, LocalTime open_time) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+        this.open_time = open_time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalTime getOpen_time() {
+        return open_time;
+    }
+
+    public void setOpen_time(LocalTime open_time) {
+        this.open_time = open_time;
+    }
+}
