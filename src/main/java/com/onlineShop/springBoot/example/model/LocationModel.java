2aa70d653c729790dd026170c4765b727bbd6e88
parham <parhamsaghazadeh@gmail.com>
2025-10-16 14:32:53 +0330

    add LocationModel

---
+++ src/main/java/com/onlineShop/springBoot/example/model/LocationModel.java
@@ -0,0 +1,8 @@
+package com.onlineShop.springBoot.example.model;
+
+public class LocationModel {
+    private Long id;
+    private String title;
+    private String type;
+    private String open_time;
+}
