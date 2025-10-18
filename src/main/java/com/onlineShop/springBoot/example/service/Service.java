d547d2ddbf05c73e83cee5ce438be0bbd2a88580
parham <parhamsaghazadeh@gmail.com>
2025-10-18 21:45:01 +0330

    Add service display ordered

---
+++ src/main/java/com/onlineShop/springBoot/example/service/Service.java
@@ -0,0 +1,109 @@
+package com.onlineShop.springBoot.example.service;
+
+// فرض بر این است که این کلاس‌ها از بسته (package) شما وارد شده‌اند
+import com.onlineShop.springBoot.example.db.ResultSetHandler;
+import com.onlineShop.springBoot.example.db.CrudRepository; // <-- اضافه شده برای استفاده در سرویس
+import com.onlineShop.springBoot.example.db.DatabaseConnection; // <-- اضافه شده برای مدیریت اتصال
+import com.onlineShop.springBoot.example.entity.*;
+import com.onlineShop.springBoot.example.model.Converter;
+
+import lombok.extern.slf4j.Slf4j; // <-- اضافه شده برای لاگ‌گیری
+import java.sql.Connection; // <-- اضافه شده برای مدیریت اتصال
+import java.util.List;
+import java.time.Year;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.ArrayList;
+
+
+@Slf4j
+public class Service {
+    Converter converter = new Converter();
+
+    public static final ResultSetHandler<DisplayOrdered> displayOrderedHandler = resultSet -> {
+        DisplayOrdered displayOrdered = new DisplayOrdered();
+
+        Product product = new Product();
+        product.setName(resultSet.getString("product_name"));
+        product.setBrand(resultSet.getString("product_brand"));
+        product.setModel(resultSet.getString("product_model"));
+        product.setMade_in(resultSet.getString("product_made_in"));
+        product.setYear_of_manufacture(Year.of(resultSet.getInt("year_of_manufacture")));
+        product.setDesign(resultSet.getString("product_design"));
+        product.setPrice(resultSet.getBigDecimal("product_price"));
+
+
+        Category category = new Category();
+        category.setTitle(resultSet.getString("category_title"));
+
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setQuantity(resultSet.getInt("order_item_quantity"));
+        orderItem.setPrice(resultSet.getBigDecimal("order_item_price"));
+
+        ProductRegistration productRegistration = new ProductRegistration();
+        productRegistration.setRegistration_date(resultSet.getDate("registration").toLocalDate());
+
+        Order order = new Order();
+        order.setPayment_method(resultSet.getString("payment_method"));
+        order.setPayment_date(resultSet.getTimestamp("payment_date").toLocalDateTime());
+
+        Person person = new Person();
+        person.setName(resultSet.getString("person_name"));
+        person.setLastname(resultSet.getString("person_lastname"));
+        person.setBirthdate(resultSet.getDate("birthdate"));
+        person.setNational_id(resultSet.getString("person_national_id"));
+
+        Location location = new Location();
+        location.setTitle(resultSet.getString("location_title"));
+        location.setType(resultSet.getString("location_type"));
+        location.setOpen_time(resultSet.getTime("open_time").toLocalTime());
+
+        return displayOrdered;
+
+    };
+
+    public List<DisplayOrdered> displayOrderedList(){
+        log.info("displayOrderedList");
+        try (Connection connection = DatabaseConnection.getConnection()){
+            CrudRepository<DisplayOrdered> displayOrderedRepository = new CrudRepository<>(connection,"product", displayOrderedHandler);
+
+            return displayOrderedRepository.readAll("SELECT p.name                 AS person_name,\n" +
+                    "       p.lastname             AS person_lastname,\n" +
+                    "       p.national_id          AS person_national_id,\n" +
+                    "       c.title                AS category_title,\n" +
+                    "       po.name                AS product_name,\n" +
+                    "       po.brand               AS product_brand,\n" +
+                    "       po.model               AS product_model,\n" +
+                    "       po.made_in             AS product_made_in,\n" +
+                    "       po.year_of_manufacture AS product_year,\n" +
+                    "       po.design              AS product_design,\n" +
+                    "       po.price               AS product_price,\n" +
+                    "       o.payment_method       AS order_payment_method,\n" +
+                    "       o.payment_date         AS order_payment_date,\n" +
+                    "       orr.quantity           AS order_quantity,\n" +
+                    "       orr.price              AS order_price,\n" +
+                    "       pr.registration_date   AS product_registration_date,\n" +
+                    "       l.title                AS location_title,\n" +
+                    "       l.type                 AS location_type,\n" +
+                    "       l.open_time            AS location_open_time\n" +
+                    "From product po\n" +
+                    "         join categories c on po.category_id = c.id\n" +
+                    "          join orders_item orr on po.id = orr.product_id\n" +
+                    "          join product_registration pr on po.id = pr.product_id\n" +
+                    "          join orders o on orr.order_id = o.id\n" +
+                    "          join person p on o.person_id = p.id\n" +
+                    "          join order_location lo on o.id = lo.order_id\n" +
+                    "          join location l on lo.location_id = l.id;");
+        }catch (SQLException e){
+            log.error("displayOrderedList{}", e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+}
+
