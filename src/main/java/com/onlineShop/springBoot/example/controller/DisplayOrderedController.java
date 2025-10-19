4f8c4981da1b70f86227670d76eedd86f16f684c
parham <parhamsaghazadeh@gmail.com>
2025-10-19 21:55:29 +0330

    Add DisplayOrderedController

--- src/main/java/com/onlineShop/springBoot/example/controller/DisplayOrderedController.java
+++ src/main/java/com/onlineShop/springBoot/example/controller/DisplayOrderedController.java
@@ -1,4 +1,30 @@
 package com.onlineShop.springBoot.example.controller;
 
+import com.onlineShop.springBoot.example.entity.DisplayOrdered;
+import com.onlineShop.springBoot.example.service.Service;
+import com.onlineShop.springBoot.example.model.Converter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/display")
+@Slf4j
 public class DisplayOrderedController {
+    @Autowired
+    private Converter converter;
+
+    private Service service;
+    @GetMapping
+    public ResponseEntity<List<DisplayOrdered>> getDisplayOrdered(){
+        log.info("getDisplayOrdered()");
+        return ResponseEntity.ok(service.displayOrderedList());
+
+    }
+
 }
