bc93aa383cb65ba1756be9e4fcf1213b990ae07d
parham <parhamsaghazadeh@gmail.com>
2025-11-29 16:27:27 +0330

    Complete the code login and fixed cod

--- src/main/java/com/onlineShop/springBoot/example/service/LoginService.java
+++ src/main/java/com/onlineShop/springBoot/example/service/LoginService.java
@@ -2,6 +2,7 @@ package com.onlineShop.springBoot.example.service;
 
 
 import com.onlineShop.springBoot.example.model.USerModel;
+import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.AccessTokenResponse;
@@ -10,19 +11,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginService {
 
-    public AccessTokenResponse login (USerModel loginUser) {
-        try {
-            Keycloak keycloak = KeycloakBuilder.builder()
-                    .serverUrl("http://localhost:8180")
-                    .realm("my-realm")
-                    .clientId("my-backend")
-                    .grantType(loginUser.getUsername())
-                    .grantType(loginUser.getPassword())
-                    .build();
-            return keycloak.tokenManager().getAccessToken();
-        }catch (Exception e){
-            throw new RuntimeException("نام کاربری یا رمز اشتباس");
-        }
+    public Keycloak getKeycloak(USerModel loginUser) {
+
+        String serverUrl = "http://localhost:8180";
+        String realm = "my-realm";
+        String clientId = "my-backend";
+
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .clientId(clientId)
+                .grantType(OAuth2Constants.PASSWORD)
+                .username(loginUser.getUsername())
+                .password(loginUser.getPassword())
+                .build();
+        return keycloak;
     }
 
+    public String login(USerModel loginUser) {
+        try {
+            Keycloak keycloak = getKeycloak(loginUser);
+            System.out.println("Login successful" + loginUser.getUsername());
+            return "Successfully logged in " + loginUser.getUsername();
+        }catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Login failed" + loginUser.getUsername());
+
+            return "Login failed" + loginUser.getUsername();
+        }
+    }
 }
