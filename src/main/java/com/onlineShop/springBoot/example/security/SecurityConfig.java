26d8d3b4efdc648679cb27e1dd5a77c0134a4fc3
parham <parhamsaghazadeh@gmail.com>
2025-11-29 16:27:53 +0330

    add url

--- src/main/java/com/onlineShop/springBoot/example/security/SecurityConfig.java
+++ src/main/java/com/onlineShop/springBoot/example/security/SecurityConfig.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(
+                "http://localhost:8081/realms/my-realm/protocol/openid-connect/certs")
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,10 +26,11 @@ public class SecurityConfig {
         http
                 .csrf(cs -> cs.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/shop/entry/register").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth -> oauth.jwt());
+                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> {}));
 
         return http.build();
     }
