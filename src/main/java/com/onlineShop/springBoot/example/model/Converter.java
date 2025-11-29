53c8792f1896b0cc178c4f0e3ab68307f6097dfc
parham <parhamsaghazadeh@gmail.com>
2025-11-29 16:29:41 +0330

    delete user

--- src/main/java/com/onlineShop/springBoot/example/model/Converter.java
+++ src/main/java/com/onlineShop/springBoot/example/model/Converter.java
@@ -3,12 +3,10 @@ package com.onlineShop.springBoot.example.model;
 import com.onlineShop.springBoot.example.entity.Person;
 import com.onlineShop.springBoot.example.entity.*;
 
-import java.sql.Time;
 import java.text.DecimalFormat;
 import java.math.BigDecimal;
 
 
-import com.onlineShop.springBoot.example.entity.User;
 import org.springframework.stereotype.Component;
 
 import java.sql.Date;
@@ -19,24 +17,6 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class Converter {
-    public UserModel converterToUser(User user) {
-        UserModel userModel = new UserModel();
-        userModel.setId(user.getId());
-        userModel.setUsername(user.getUsername());
-        userModel.setPassword(user.getPassword());
-        return userModel;
-    }
-
-    public User converterToUser(UserModel userModel) {
-        if (userModel == null) {
-            return null;
-        }
-        return new User(
-                userModel.getId(),
-                userModel.getUsername(),
-                userModel.getPassword()
-        );
-    }
 
     public PersonModel converterToPerson(Person person) {
         if (person == null) {
