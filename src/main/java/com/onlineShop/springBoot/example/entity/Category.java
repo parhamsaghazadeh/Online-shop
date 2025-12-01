b2774e16af381c9db81c140c3e013b958d208b9f
parham <parhamsaghazadeh@gmail.com>
2025-12-01 19:40:53 +0330

    Add JPA annotations to Category entity

--- src/main/java/com/onlineShop/springBoot/example/entity/Category.java
+++ src/main/java/com/onlineShop/springBoot/example/entity/Category.java
@@ -1,6 +1,13 @@
 package com.onlineShop.springBoot.example.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
 
