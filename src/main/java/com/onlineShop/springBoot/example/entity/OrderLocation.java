1d9362a589df6f7620f92fb237ee48a9753ba012
parham <parhamsaghazadeh@gmail.com>
2025-12-02 22:33:42 +0330

    add JPA annotation to OrderLocation entity

--- src/main/java/com/onlineShop/springBoot/example/entity/OrderLocation.java
+++ src/main/java/com/onlineShop/springBoot/example/entity/OrderLocation.java
@@ -1,11 +1,21 @@
 package com.onlineShop.springBoot.example.entity;
 
-public class OrderLocation {
-    private Long id;
-    private Long order_id;
-    private Long location_id;
+import jakarta.persistence.*;
 
-    public OrderLocation(Long id, Long order_id, Long location_id) {
+@Entity
+@Table(name = "order_location")
+public class OrderLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order_id;
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location_id;
+
+    public OrderLocation(Long id, Order order_id, Location location_id) {
         this.id = id;
         this.order_id = order_id;
         this.location_id = location_id;
@@ -19,19 +29,19 @@ public class OrderLocation {
         this.id = id;
     }
 
-    public Long getOrder_id() {
+    public Order getOrder_id() {
         return order_id;
     }
 
-    public void setOrder_id(Long order_id) {
+    public void setOrder_id(Order order_id) {
         this.order_id = order_id;
     }
 
-    public Long getLocation_id() {
+    public Location getLocation_id() {
         return location_id;
     }
 
-    public void setLocation_id(Long location_id) {
+    public void setLocation_id(Location location_id) {
         this.location_id = location_id;
     }
 }
