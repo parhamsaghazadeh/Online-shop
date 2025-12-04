9386ee411bba9c5e8416f55a2e8df26677395044
parham <parhamsaghazadeh@gmail.com>
2025-12-04 14:47:59 +0330

    fixed code ProductModel

--- src/main/java/com/onlineShop/springBoot/example/model/ProductModel.java
+++ src/main/java/com/onlineShop/springBoot/example/model/ProductModel.java
@@ -1,6 +1,6 @@
 package com.onlineShop.springBoot.example.model;
 
-import com.onlineShop.springBoot.example.entity.Category;
+import java.math.BigDecimal;
 
 public class ProductModel {
     private Long id;
@@ -8,10 +8,10 @@ public class ProductModel {
     private String brand;
     private String model;
     private String made_in;
-    private String year_of_manufacture;
+    private int year_of_manufacture;
     private String design;
-    private String price;
-    private Long category_id;
+    private BigDecimal price;
+    private Long categoryId;
 
 
     public Long getId() {
@@ -54,11 +54,11 @@ public class ProductModel {
         this.made_in = made_in;
     }
 
-    public String getYear_of_manufacture() {
+    public int getYear_of_manufacture() {
         return year_of_manufacture;
     }
 
-    public void setYear_of_manufacture(String year_of_manufacture) {
+    public void setYear_of_manufacture(int year_of_manufacture) {
         this.year_of_manufacture = year_of_manufacture;
     }
 
@@ -70,19 +70,19 @@ public class ProductModel {
         this.design = design;
     }
 
-    public String getPrice() {
+    public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
-    public Long getCategory_id() {
-        return category_id;
+    public Long getCategoryId() {
+        return categoryId;
     }
 
-    public void setCategory_id(Long category_id) {
-        this.category_id = category_id;
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
     }
 }
